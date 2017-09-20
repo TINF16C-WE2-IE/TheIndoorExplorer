12f7a7fef2dd0c75d620a666053a39d1438e1e15
SCHECTER\Freddi <unknown@bla.de>
2017-09-20 02:17:36 +0200

    moved SvgEditComponent-Template into own component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SvgEditComponent } from './svg-edit/svg-edit.component';
 
 export const routes: Routes = [
 
@@ -13,6 +14,10 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'editor',
+        component: SvgEditComponent,
     },
 ];
 
