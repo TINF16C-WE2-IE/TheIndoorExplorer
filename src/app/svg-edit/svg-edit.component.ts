aa21f597f7c4aae91ad74078b415cd85e718f671
robin <robin@nubenum.de>
2017-10-09 21:36:29 +0200

    update door rotation on-the-fly, avoid vertex merge on move, rename lines to walls, misc fixes

--- src/app/svg-edit/svg-edit.component.ts
+++ src/app/svg-edit/svg-edit.component.ts
@@ -9,14 +9,14 @@ export class SvgEditComponent implements OnInit {
 
     structure = [
         {
-            'lines' : [],
+            'walls' : [],
             'doors' : []
         }
     ];
-    nowDrawing = false;
     moveStart = [];
+    movingObjects = [];
     currentPos = [];
-    layer = 0;
+    level = 0;
     tool = 0;
 
     constructor() {
@@ -25,10 +25,9 @@ export class SvgEditComponent implements OnInit {
     ngOnInit() {
     }
 
-
     gridSnap(arr, threshold?: number) {
         if (!threshold) threshold = 10;
-       return [Math.round(arr[0] / threshold) * threshold, Math.round(arr[1] / threshold) * threshold];
+        return [Math.round(arr[0] / threshold) * threshold, Math.round(arr[1] / threshold) * threshold];
     }
 
     mouseGridSnap(evt) {
@@ -39,82 +38,120 @@ export class SvgEditComponent implements OnInit {
         return a[0] === b[0] && a[1] === b[1];
     }
 
-    isExistingVertex(pos): boolean {
-        this.structure[this.layer].lines.forEach(line => {
-            if (this.equalsXY(line.a, pos)) return true;
-            if (this.equalsXY(line.b, pos)) return true;
+    getWallsAtVertex(pos) {
+        const wallsAtVertex = [];
+        this.structure[this.level].walls.forEach(wall => {
+            if (this.equalsXY(wall.a, pos)) wallsAtVertex.push(wall);
+            if (this.equalsXY(wall.b, pos)) wallsAtVertex.push(wall);
         });
-        return false;
+        return wallsAtVertex;
     }
 
-    splitLine(line, splitPos) {
-        const a = line.a;
-        line.a = splitPos;
-        this.structure[this.layer].lines.push({'a': a, 'b': splitPos});
-        console.log(this.structure[this.layer].lines);
+    isExistingVertex(pos): boolean {
+        return this.getWallsAtVertex(pos).length !== 0;
     }
 
-    isOnExistingLine(pos): number {
-        let rotation = -1;
-        this.structure[this.layer].lines.forEach(line => {
-            if (!this.equalsXY(line.a, line.b)) {
-                const xdiff = line.b[0] - line.a[0];
-                const ydiff = line.b[1] - line.a[1];
+    getRotation(pos): number {
+        const walls = this.getWallsAtVertex(pos);
+        let rotations = 0;
+        walls.forEach(wall => {
+            if (!this.equalsXY(wall.a, wall.b)) {
+                const xdiff = wall.b[0] - wall.a[0];
+                const ydiff = wall.b[1] - wall.a[1];
 
                 if (xdiff === 0) {
-                    const shift = (this.currentPos[1] - line.a[1]) / ydiff;
-                    if (this.currentPos[0] === line.a[0] &&  shift >= 0 && shift <= 1) {
-                        this.splitLine(line, this.currentPos);
-                        rotation = 90;
+                    rotations += 90;
+                }
+                else {
+                    const rotation = Math.atan(ydiff / xdiff) / Math.PI * 180;
+                    rotations += rotation;
+                }
+            }
+        });
+        return rotations / walls.length;
+    }
+
+    splitLine(wall, splitPos) {
+        const a = wall.a;
+        wall.a = splitPos;
+        this.structure[this.level].walls.push({'a': a, 'b': splitPos});
+        console.log(this.structure[this.level].walls);
+    }
+
+    getNearbyLine(pos) {
+        let foundLine = null;
+        this.structure[this.level].walls.forEach(wall => {
+            if (!this.equalsXY(wall.a, wall.b)) {
+                const xdiff = wall.b[0] - wall.a[0];
+                const ydiff = wall.b[1] - wall.a[1];
+
+                if (xdiff === 0) {
+                    const shift = (this.currentPos[1] - wall.a[1]) / ydiff;
+                    if (this.currentPos[0] === wall.a[0] &&  shift >= 0 && shift <= 1) {
+                        foundLine = wall;
                     }
-                } else {
-                    const shift = (this.currentPos[0] - line.a[0]) / xdiff;
+                }
+                else {
+                    const shift = (this.currentPos[0] - wall.a[0]) / xdiff;
                     if (shift >= 0 && shift <= 1) {
-                        const deducedY = ydiff * shift + line.a[1];
+                        const deducedY = ydiff * shift + wall.a[1];
                         if (this.equalsXY(this.gridSnap([this.currentPos[0], deducedY], 20), this.gridSnap(this.currentPos, 20))) {
-                            this.splitLine(line, this.currentPos);
-                            rotation = Math.atan(ydiff / xdiff) / Math.PI * 180;
-                            if (rotation < 0) rotation += 360;
+                            foundLine = wall;
                         }
                     }
                 }
             }
         });
-        return rotation;
+        return foundLine;
     }
 
     moveVertex(evt: MouseEvent) {
-        const lines = this.structure[this.layer].lines;
+        const walls = this.structure[this.level].walls;
+        const doors = this.structure[this.level].doors;
         if (evt.buttons) {
-            if (this.moveStart.length && !this.equalsXY(this.moveStart, this.currentPos)) {
-                console.log(this.moveStart, this.currentPos);
-                lines.forEach(line => {
-                    if (this.equalsXY(line.a, this.moveStart)) line.a = this.currentPos;
-                    if (this.equalsXY(line.b, this.moveStart)) line.b = this.currentPos;
+            if (this.moveStart.length) {
+                this.movingObjects.forEach(obj => {
+                    if (obj.pos) obj.pos = this.currentPos;
+                    if (obj.a) {
+                        if (this.equalsXY(obj.a, this.moveStart)) obj.a = this.currentPos;
+                        if (this.equalsXY(obj.b, this.moveStart)) obj.b = this.currentPos;
+                    }
+                });
+            }
+            else {
+                walls.forEach(wall => {
+                    if (this.equalsXY(wall.a, this.currentPos) || this.equalsXY(wall.b, this.currentPos)) {
+                        this.movingObjects.push(wall);
+                    }
+                });
+                doors.forEach(door => {
+                    if (this.equalsXY(door.pos, this.currentPos)) this.movingObjects.push(door);
                 });
             }
             this.moveStart = this.currentPos;
         }
-        else if (!evt.buttons) {
+        else {
             this.moveStart = [];
+            this.movingObjects = [];
         }
     }
 
     endMoveVertex(evt: MouseEvent) {
         this.moveStart = [];
+        this.movingObjects = [];
     }
 
     draw(evt: MouseEvent) {
-        const lines = this.structure[this.layer].lines;
+        const walls = this.structure[this.level].walls;
         if (evt.buttons) {
             if (this.moveStart.length) {
-                lines[lines.length - 1].b = this.currentPos;
+                walls[walls.length - 1].b = this.currentPos;
             } else {
-                lines.push({'a': this.currentPos, 'b': this.currentPos});
+                walls.push({'a': this.currentPos, 'b': this.currentPos});
+                this.moveStart = this.currentPos;
             }
-            this.moveStart = this.currentPos;
         }
-        else if (!evt.buttons) {
+        else {
             this.moveStart = [];
         }
     }
@@ -124,14 +161,15 @@ export class SvgEditComponent implements OnInit {
     }
 
     setDoor(evt: MouseEvent) {
-        const lines = this.structure[this.layer].lines;
-        const doors = this.structure[this.layer].doors;
+        const walls = this.structure[this.level].walls;
+        const doors = this.structure[this.level].doors;
 
-        let rotation: number;
-        if (this.isExistingVertex(this.currentPos) || (rotation = this.isOnExistingLine(this.currentPos)) >= 0) {
-           doors.push({'pos': this.currentPos, 'id': doors.length, 'label': 'Door', 'rot': rotation});
+        let wall = null;
+        if (this.isExistingVertex(this.currentPos) || (wall = this.getNearbyLine(this.currentPos))) {
+            if (wall) this.splitLine(wall, this.currentPos);
+            doors.push({'pos': this.currentPos, 'id': doors.length, 'label': 'Door'});
         }
-        console.log(doors, rotation);
+        console.log(doors);
     }
 
     doNothing(evt: MouseEvent) {
