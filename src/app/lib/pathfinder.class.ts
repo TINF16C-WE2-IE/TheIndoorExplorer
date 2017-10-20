208cd8e28bb4758e9ab1e9122fde6dfdaecf2c71
Frederik Erhardt <erhardt@siriusonline.de>
2017-10-20 16:34:45 +0200

    added optimal path-tracking. Pathfinding in a given linked node system is almost complete by now.

--- src/app/lib/pathfinder.class.ts
+++ src/app/lib/pathfinder.class.ts
@@ -13,28 +13,26 @@ export class Pathfinder {
 
         if (from === to || nodes.indexOf(from) === nodes.indexOf(to)) return [from];
 
-        const resultList = [];
+        // open/closed nodes for pathfinding
         const openList = [];
         const closedList = [];
 
 
-        // assign initial costs for nodes
+        // assign initial costs for nodes, and create their parents for backtracking the best path.
         const costs = new Array();
+        const parents = new Array();
         for (let n = 0; n < nodes.length; n++) {
             costs.push( nodes[n] === from ? 0 : Number.MAX_VALUE);
+            parents.push(nodes[n]);
         }
-
         closedList.push(from);
-        this.calculateCosts(nodes, costs, openList, closedList, to);
 
-        // todo. based on costs, search for optimal path.
-
-        return null;
+        console.log('pathfinder is calculating the best path over linked nodes. (nodes, from, to)', nodes, from, to);
+        return this.calculatePath(nodes, costs, parents, openList, closedList, to);
     }
 
-    private calculateCosts(nodes: PathNode[], costs: number[], openList: PathNode[], closedList: PathNode[], end: PathNode): number[] {
-        console.log('at the beginning of the findpath: (nodes, costs, open, closed, end)',
-                      nodes, costs, openList, closedList, end);
+    private calculatePath(nodes: PathNode[], costs: number[], parents: PathNode[],
+                            openList: PathNode[], closedList: PathNode[], end: PathNode): PathNode[] {
 
         let min = Number.MAX_VALUE;
         let curCost = min;
@@ -43,13 +41,13 @@ export class Pathfinder {
         for (const cl of closedList) {
             for (let j = 0; j < cl.links.length; j++) {
 
-
                 // calculate cost
                 curCost = costs[nodes.indexOf(cl)] + cl.costs[j];
 
                 // take over the minimal cost
                 if (curCost < costs[nodes.indexOf(cl.links[j])]) {
                     costs[nodes.indexOf(cl.links[j])] = curCost;
+                    parents[nodes.indexOf(cl.links[j])] = cl;
                 }
 
 
@@ -60,42 +58,44 @@ export class Pathfinder {
                         openList.push(cl.links[j]);
                     }
 
-                    // select the global path with minimal cost DEPRECTAED maybe
+                    // from all the open links, select the best link with minimal cost
                     if (curCost < min) {
                         min = curCost;
                         minClNode = cl;
                         minLinkIndex = j;
                     }
-
-                    console.log('available next node: from ', cl.x, '/', cl.y, ' to ', cl.links[j].x,
-                                  '/', cl.links[j].y, ' cost: ', curCost);
                 }
             }
         }
 
-        console.log('before: (open, closed)', openList, closedList);
         openList = openList.splice(openList.indexOf(minClNode.links[minLinkIndex]), 1);
         closedList.push(minClNode.links[minLinkIndex]);
-        console.log('after: (open, closed)', openList, closedList);
 
         if (openList.length > 0 && closedList.indexOf(end) < 0) {
-            this.calculateCosts(nodes, costs, openList, closedList, end);
+            return this.calculatePath(nodes, costs, parents, openList, closedList, end);
         } else {
-            console.log('got your cost calculation. (nodes, costs, closed, open, end)', nodes, costs, closedList, openList, end);
-        }
-        // now we got all costs.
 
-        return costs;
+            // got the costs over the nodes. now back-track.
+            // start at end node and backtrack over the parent for each node.
+            // while the last node in backtracking-path has a valid parent (means: not themselves), go on.
+            const resultPath = new Array();
+            resultPath.push(end);
+            while (parents[nodes.indexOf(resultPath[resultPath.length - 1])] !== resultPath[resultPath.length - 1]) {
+                resultPath.push(parents[nodes.indexOf(resultPath[resultPath.length - 1])]);
+            }
+            resultPath.reverse();
+            return resultPath;
+        }
     }
 
     public debugTest(): void {
         const n1 = new PathNode(100, 100);
         const n2 = new PathNode(200, 200);
-        const n3 = new PathNode(300, 300);
+        const n3 = new PathNode(300, 500);
         const n4 = new PathNode(400, 400);
         const n5 = new PathNode(500, 500);
         const n6 = new PathNode(600, 600);
-        const n7 = new PathNode(700, 700);
+        const n7 = new PathNode(200, 100);
         const n8 = new PathNode(800, 800);
         const n9 = new PathNode(900, 900);
         const n10 = new PathNode(1000, 1000);
@@ -106,16 +106,20 @@ export class Pathfinder {
             nn2.addLinkTo(nn1, dist);
         };
 
+        // creates a simple path with 2 shortcuts (1<->4, 4<->7 and 3<->7)
         link(n1, n2);
-        link(n1, n3);
         link(n2, n3);
-        link(n1, n4);
         link(n3, n4);
-        link(n4, n6);
-        link(n1, n6);
-        link(n3, n6);
+        link(n4, n5);
+        link(n5, n6);
         link(n6, n7);
+        link(n1, n4);
+        link(n3, n7);
+        link(n4, n7);
 
-        this.findPathFromTo([n1, n2, n3, n4, n5, n6, n7, n8, n9, n10], n1, n7);
+        // in this special example: pathfinder should take the path over 1-4-7
+        // because due to shortcuts, its faster than moving over 1-2-3-7.
+
+        console.log('path-result: ', this.findPathFromTo([n1, n2, n3, n4, n5, n6, n7, n8, n9, n10], n1, n7));
     }
 }
