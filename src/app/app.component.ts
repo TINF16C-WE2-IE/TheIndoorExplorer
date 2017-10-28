cb5df7d0a45ee96f96dc5cd79ca6941bade6351c
ByteCommander <bytecommander@users.noreply.github.com>
2017-10-28 15:26:29 +0200

    Fix login detection (move to app.component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModelService } from './svc/model.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'app works!';
+
+    constructor(private modelSvc: ModelService) {
+    }
 
     scrollTop() {
         window.scrollTo(0, 0);
+    }
+
+    ngOnInit() {
+        this.modelSvc.loadUserInfo();
     }
 }
--- src/app/home/main.component.ts
+++ src/app/home/main.component.ts
@@ -1,18 +1,10 @@
-import { RequestService } from '../svc/request.service';
-import { ModelService } from '../svc/model.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
-
-  constructor(private modelSvc: ModelService) { }
-
-  ngOnInit() {
-      this.modelSvc.loadUserInfo();
-  }
+export class MainComponent {
 
 }
